b42f47a051e25e3b69a9f90bce960cfb2273a2b1
sho <ikeguchi.sho@gmail.com>
2021-07-23 23:15:48 +0900

    Improve: Styling & Theming

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -12,6 +12,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('MealRecipes'),
       ),
       body: GridView(
+        padding: EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
