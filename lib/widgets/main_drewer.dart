2cf59e571770f57eb2ddcd60be26c1ae36e7e290
sho <ikeguchi.sho@gmail.com>
2021-08-08 18:24:23 +0900

    Add: Links to the Drawer

--- lib/widgets/main_drewer.dart
+++ lib/widgets/main_drewer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../screens/filters_screen.dart';
+
 class MainDrawer extends StatelessWidget {
-  Widget buildListTitle(String title, IconData icon) {
+  Widget buildListTitle(String title, IconData icon, Function tapHandler) {
     return ListTile(
       leading: Icon(
         icon,
@@ -15,9 +17,7 @@ class MainDrawer extends StatelessWidget {
           fontWeight: FontWeight.bold,
         ),
       ),
-      onTap: () {
-        // ...
-      },
+      onTap: tapHandler,
     );
   }
 
@@ -44,14 +44,12 @@ class MainDrawer extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          buildListTitle(
-            'Meals',
-            Icons.restaurant,
-          ),
-          buildListTitle(
-            'Filters',
-            Icons.settings,
-          ),
+          buildListTitle('Meals', Icons.restaurant, () {
+            Navigator.of(context).pushNamed('/');
+          }),
+          buildListTitle('Filters', Icons.settings, () {
+            Navigator.of(context).pushNamed(FiltersScreen.routeName);
+          }),
         ],
       ),
     );
