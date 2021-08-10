bf3d0327135cb8d1a0a3d6b73c6b0a0bcb7235f3
sho <ikeguchi.sho@gmail.com>
2021-08-10 23:33:03 +0900

    Add: meal item delete button

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -89,6 +89,11 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.delete),
+          onPressed: () {
+            Navigator.of(context).pop(mealId);
+          }),
     );
   }
 }
